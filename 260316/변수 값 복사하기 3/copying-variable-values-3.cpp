ff3f5e042c77d3f94c124607b5830037be336cf6
jye <kelly0819.han1@gmail.com>
2026-03-16 13:48:53 +0900

    Title: 변수 값 복사하기 3, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 260316/변수 값 복사하기 3/copying-variable-values-3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a=1, b=5, c=3;
+    int tmp;
+    a = c;
+    a = a+c;
+    b = b-c;
+
+
+
+    cout << a << "\n";
+    cout << b << "\n";
+    cout << c << endl;
+
+    return 0;
+}
