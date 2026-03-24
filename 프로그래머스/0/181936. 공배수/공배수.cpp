a7d004e0878e3cac766b891b659fed6dca891be2
jye <kelly0819.han1@gmail.com>
2026-03-24 14:38:04 +0900

    [level 0] Title: 공배수, Time: 0.01 ms, Memory: 3.66 MB -BaekjoonHub

---
+++ 프로그래머스/0/181936. 공배수/공배수.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int solution(int number, int n, int m) {
+    if (number % n == 0 && number % m == 0) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
